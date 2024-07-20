36dacf9d8d8ba2c20ac82535dc14b5f1304ec96f
nisreenbouta <2010213598@ogrenci.karabuk.edu.tr>
2024-07-20 16:21:10 +0300

    hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,11 @@
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-4">
 
-      <h2 className=" text-4xl font-bold text-blue-300 ">Hello
+      <Hero/>
 
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt a asperiores velit, distinctio quos, quibusdam nesciunt numquam autem pariatur facilis possimus? Pariatur ad tenetur distinctio fugit quidem nam! Perspiciatis, nisi?
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur saepe, officiis molestiae nobis, at reiciendis ea, placeat alias quas ipsam nesciunt earum quasi. Et non saepe omnis minima fuga itaque. 
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quas, quo officiis ipsum cum ratione a incidunt commodi expedita suscipit labore quibusdam animi, harum repellendus quidem beatae laborum atque blanditiis itaque.
-        Incidunt quod perferendis cumque amet aspernatur. Sunt nostrum libero cumque earum iste voluptatem dolor alias deserunt quaerat aut ipsa enim quasi harum assumenda molestiae fugiat nulla impedit, voluptatibus magnam voluptatum.
-        Et maxime necessitatibus beatae nulla, facere, culpa nisi corporis, sit mollitia pariatur similique repellendus sequi maiores ea esse atque cum excepturi nihil? Ratione, quia. Nemo in quidem placeat aspernatur incidunt.
-        Cupiditate, sed, itaque ratione suscipit facere accusantium optio, doloribus fugit id animi necessitatibus architecto. Autem rem deleniti quas fuga, cumque saepe et alias. Officia iure numquam autem quas sit pariatur.
-        Voluptatibus placeat eveniet sapiente praesentium molestias, vel amet quos accusamus doloribus eligendi pariatur, atque ad doloremque at ab. Mollitia odio magni asperiores ea, consequuntur sunt voluptate facere explicabo in dolorem?
-        Ullam perferendis sequi quisquam, nobis dolorum sint repellendus itaque quia amet aspernatur maiores necessitatibus dolor magni laudantium odio est minus asperiores aperiam, exercitationem quaerat tempora enim velit, adipisci architecto. Eum.
-        Voluptate porro expedita animi odit iure! Quas eaque reprehenderit nam soluta, quibusdam deleniti esse ratione perspiciatis vel, accusantium vitae, error impedit recusandae enim veniam maxime unde adipisci earum necessitatibus facere.
-        Sit laudantium non qui temporibus corrupti quibusdam ad unde iste, voluptatem quidem illo aut cumque sapiente maxime quaerat excepturi reiciendis, minima iure eum dicta quo voluptates! Dicta aliquam repellat illum?
-        Eligendi explicabo dolores laborum ratione maiores est. Quos maxime architecto, ipsa quaerat perspiciatis hic libero earum voluptatem amet inventore nam culpa omnis dolores consequuntur veniam tempore odio voluptates laboriosam quisquam.
-        Corporis odio itaque similique saepe, asperiores suscipit laboriosam vel voluptates obcaecati voluptas. Error exercitationem tempore quod? Aperiam mollitia deleniti officiis! Porro voluptatum nesciunt commodi cumque excepturi, aut harum molestiae obcaecati.
-        In enim sit, numquam veritatis maiores dolorum molestias. Dolorem iusto temporibus non eligendi ea quaerat ratione at labore, quis fuga vel repudiandae consequuntur, accusamus officia recusandae! Asperiores nam nisi voluptatibus?
-        Dignissimos corporis, nisi iure, ipsam similique nam aspernatur commodi cupiditate tempora odio, rem sapiente dicta quaerat nemo assumenda odit eos dolore. Exercitationem iste quidem minima corporis voluptatem quis adipisci tenetur.
-        Consectetur totam hic suscipit unde animi natus, explicabo obcaecati, sint consequatur eum doloremque. Dicta perferendis earum consequatur enim molestias dolorem sapiente similique hic rerum. Laudantium nemo dolorem quam consequuntur vel.
-        Officiis eligendi id expedita doloribus tempora mollitia magnam vero rem at, qui harum quam, dolor, culpa quia porro iste. Excepturi repudiandae odio non, reiciendis possimus nesciunt quasi culpa inventore corporis?
-        Optio illum, nulla cumque assumenda ab corporis ipsam deserunt id doloremque quis error doloribus. Consequuntur voluptate officia velit, nesciunt similique esse, inventore placeat maiores officiis praesentium dolore exercitationem, sint mollitia.
-        Sequi delectus ut numquam sit vitae molestiae impedit, in ipsum assumenda! Necessitatibus magnam eaque numquam est provident sunt officiis? Dolorum odio quam dolores vel. Sapiente inventore odit tempora non aut?
-        Nisi, ea voluptatem nemo voluptates sed nobis distinctio aliquam doloremque quasi odio et repellendus? Illum voluptatibus laborum hic assumenda molestiae eaque unde, perferendis corrupti pariatur odio eos fugiat! In, incidunt.
-        Cupiditate earum facere veniam aliquid odio fugiat labore impedit ipsam, id commodi aperiam? Eveniet inventore facilis optio ipsum, voluptatum nam at eum necessitatibus quia omnis non excepturi corporis praesentium nostrum?
-        Provident sunt possimus aperiam eius cupiditate, perferendis sed libero a modi id eveniet odit voluptatem dolores nihil quibusdam veritatis nulla voluptatum voluptas enim nisi sapiente? Beatae omnis optio fugit harum.
-      </h2>
     </main>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div className="w-full z-10 sticky top-0 mt-2 rounded-lg py-4 px-56 mx-auto bg-white/30 backdrop-blur">
-        <div className="flex flex-row justify-between px-4 items-center">
-            <h2 className=' font-bold font-arsenal tracking-wide hover:text-pink-400 duration-150 ease-linear cursor-pointer'>About</h2>
-            <h2 className=' font-light font-vt323 text-4xl tracking-wide  hover:text-pink-400 duration-150 ease-linear cursor-pointer'>Nisreen Bouta</h2>
-            <h2 className=' font-bold font-arsenal tracking-wide hover:text-pink-400 duration-150 ease-linear cursor-pointer'>Projects</h2>
+    <div className="w-full z-50 sticky top-0 mt-2 rounded-lg py-4 md:px-56 mx-auto bg-white/30 backdrop-blur">
+        <div className="flex flex-row justify-between px-4 items-center md:text-lg text-sm">
+            <a href='#about'>
+                <h2 className=' font-bold font-arsenal tracking-wide hover:text-pink-400 duration-150 ease-linear cursor-pointer'>About</h2>
+            </a>
+            <h2 className=' font-light font-vt323 text-xl md:text-4xl tracking-wide  hover:text-pink-400 duration-150 ease-linear cursor-pointer'>Nisreen Bouta</h2>
+            <a href='#projects'>
+                <h2 className=' font-bold font-arsenal tracking-wide hover:text-pink-400 duration-150 ease-linear cursor-pointer'>Projects</h2>
+            </a>
         </div>
     </div>
 
