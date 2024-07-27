9f42825e75d1a6e4a1e0da785efdca443a1f55a2
nisreenbouta <2010213598@ogrenci.karabuk.edu.tr>
2024-07-27 23:24:28 +0300

    layout - updated

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,9 @@
 import { Inter } from "next/font/google";
-import { VT323, Arsenal } from "next/font/google";
+
 import "./globals.css";
 import Header from "@/components/Header";
 const inter = Inter({ subsets: ["latin"] });
-const arsenal = Arsenal({weight: "400"});
-const vt323 = VT323({ weight: "400" });
+
 
 export const metadata = {
   title: "Nisreen Bouta | Full-Stack Developer",
